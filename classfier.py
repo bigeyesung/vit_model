7138c5c8a5fbfe4c9515ee94a9b0d8dc5e1595f5
bigeyesung <sungchenhsi@gmail.com>
2021-06-05 22:40:23 +0800

    refactor code

--- classfier.py
+++ classfier.py
@@ -1,8 +1,6 @@
-#%%
 import os
 import tensorflow as tf
 import numpy as np
-# import matplotlib.pyplot as plt
 from tensorflow.keras import backend, optimizers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import GaussianNoise, Dense
@@ -12,8 +10,6 @@ from tensorflow.python.client import device_lib
 from vit_keras import vit
 from utils import*
 import collections
-# from tensorflow.compat.v1 import ConfigProto
-# from tensorflow.compat.v1 import InteractiveSession
 
 class ThreeGroupsClassfier():
     def __init__(self):
@@ -27,6 +23,7 @@ class ThreeGroupsClassfier():
         self.train_generator=None 
         self.validation_generator=None 
         self.test_generator=None
+        self.curDir = os.path.dirname(os.path.realpath(__file__))
         self.DataAugment()
         self.InitModel()
 
@@ -51,6 +48,10 @@ class ThreeGroupsClassfier():
         self.model.add(noise)
         self.model.add(vit_model)
         self.model.add(head)
+        self.model.compile(
+            optimizer=optimizers.Adam(),
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"])  
 
     def DataAugment(self):
         train_datagen = ImageDataGenerator(
@@ -85,13 +86,8 @@ class ThreeGroupsClassfier():
     
     def Train(self):
         backend.clear_session()
-        # vit_model = vit.vit_l32(
-        #     image_size=TARGETSIZE,
-        #     pretrained=True,
-        #     include_top=False,
-        #     pretrained_top=False)
-        # print(len(vit_model.layers))
-        # print(vit_model.layers)
+        filepath="model_{epoch:02d}-{val_loss:.2f}.hdf5"
+        saveDir = self.curDir+"/models"
 
         reduceLr = ReduceLROnPlateau(
             monitor='val_accuracy',
@@ -101,46 +97,22 @@ class ThreeGroupsClassfier():
             mode='max',
             min_lr=0.0001)
 
-        filepath="model_{epoch:02d}-{val_loss:.2f}.hdf5"
-        save_dir = "/home/chenhsi/Projects/Xihelm/models"
-        checkpoint = ModelCheckpoint(os.path.join(save_dir, filepath), 
-                                        monitor='val_loss',#val_loss
-                                        verbose=1,
-                                        save_best_only=False, 
-                                        save_weights_only=False)
+        checkpoint = ModelCheckpoint(os.path.join(saveDir, filepath), 
+            monitor='val_loss',
+            verbose=1,
+            save_best_only=False, 
+            save_weights_only=False) 
 
-        # finetune_at = 28
-        # # fine-tuning
-        # for layer in vit_model.layers[:finetune_at - 1]:
-        #     layer.trainable = False
-        # num_classes = len(validation_generator.class_indices)
-        # # Add GaussianNoise layer for robustness
-        # noise = GaussianNoise(0.01, input_shape=(TARGETSIZE, TARGETSIZE, 3))
-        # # Classification head
-        # head = Dense(num_classes, activation="softmax")
-        # model = Sequential()
-        # model.add(noise)
-        # model.add(vit_model)
-        # model.add(head)
-        self.model.compile(optimizer=optimizers.Adam(),
-                    loss="sparse_categorical_crossentropy",
-                    metrics=["accuracy"])                      
         history = self.model.fit(
-                                self.train_generator,
-                                epochs=40,
-                                validation_data=self.validation_generator,
-                                verbose=1, 
-                                shuffle=True,
-                                # callbacks=[
-                                #             reduceLr,
-                                #             checkpoint]
-                                callbacks=None
-                                )
-        #save model
-        # model.save('/home/chenhsi/Projects/Xihelm/myModel.h5')
-        # model.save_weights('/home/chenhsi/Projects/Xihelm/myModel_weights.h5')
-        # modelSave=keras.models.load_model('/home/chenhsi/Projects/Xihelm/models/myModel')
-        # model.load_weights('/home/chenhsi/Projects/Xihelm/models/model_02-0.96.hdf5')
+            self.train_generator,
+            epochs=40,
+            validation_data=self.validation_generator,
+            verbose=1, 
+            shuffle=True,
+            callbacks=[
+                        reduceLr,
+                        checkpoint]
+            )
 
         history_dict = history.history
         loss_values = history_dict["loss"]
@@ -148,7 +120,6 @@ class ThreeGroupsClassfier():
         acc_values = history_dict["accuracy"]
         val_acc_values = history_dict["val_accuracy"]
         epochs = range(1, len(history_dict["accuracy"]) + 1)
-
         plt.plot(epochs, loss_values, "bo", label="train")
         plt.plot(epochs, val_loss_values, "b", label="valid")
         plt.title("Loss")
@@ -156,7 +127,6 @@ class ThreeGroupsClassfier():
         plt.ylabel("Loss")
         plt.legend()
         plt.show()
-
         plt.plot(epochs, acc_values, "bo", label="train")
         plt.plot(epochs, val_acc_values, "b", label="valid")
         plt.title("Accuracy")
@@ -164,23 +134,23 @@ class ThreeGroupsClassfier():
         plt.ylabel("Accuracy")
         plt.legend()
         plt.show()
+
         #predict image groups
         self.PredictGroups()
-
         print("best val_acc:", np.max(val_acc_values), "epoch:", np.argmax(val_acc_values))
         print("best val_loss:", np.min(val_loss_values), "epoch:", np.argmin(val_loss_values))
         test_loss, test_acc = self.model.evaluate(self.test_generator)
         print("Test Accuracy:", test_acc)
-        print("==end==")
+        print("==Train end==")
 
     def PredictGroups(self):
         folders=[
-            '/home/chenhsi/Projects/Xihelm/input/test/animals',
-            '/home/chenhsi/Projects/Xihelm/input/test/buildings',
-            '/home/chenhsi/Projects/Xihelm/input/test/landscapes'
+            self.curDir+ '/input/test/animals',
+            self.curDir+ '/input/test/buildings',
+            self.curDir+ '/input/test/landscapes'
         ]
         for folder in folders:
-            print("this folder: ",folder)
+            # print("this folder: ",folder)
             arr=[]
             GrpTable={}
             for imgFile in os.listdir(folder):
@@ -196,9 +166,16 @@ class ThreeGroupsClassfier():
             for groupInd in counter:
                 GrpTable[detectionGroups[groupInd]]=counter[groupInd]
             print(GrpTable)
+
     def Evaluate(self, filePath):
-        self.InitModel()
-        self.model.load_weights(filePath)
-        #prediction
-        self.PredictGroups()
+        if os.path.isfile(filePath):
+            self.InitModel()
+            self.model.load_weights(filePath)
+            #prediction
+            self.PredictGroups()
+            test_loss, test_acc = self.model.evaluate(self.test_generator)
+            print("Test Accuracy:", test_acc)
+            print("==Evaluation end==")
+        else:
+            print("model filePath is not accessible")
 
