6cae609c0db3a125f8c00f7d08f1d19e5e9deb1e
bigeyesung <sungchenhsi@gmail.com>
2021-06-09 22:03:24 +0800

    main.py: main script

---
+++ main.py
@@ -0,0 +1,6 @@
+#%%
+from classfier import*
+if __name__ == "__main__":
+    classfier=ThreeGroupsClassfier()
+    //classfier.Train()
+    classfier.Evaluate('/home/chenhsi/Projects/Xihelm/models/model_26-0.04.hdf5')
